8154577372ed9ab9a2139a2db65169d999c364be
terrys48 <lp.kostakis@yahoo.gr>
2022-11-29 20:01:55 +0200

    impl saveTicket, deleteTicket

--- src/main/java/eu/advantage/fibernow/service/TicketServiceImpl.java
+++ src/main/java/eu/advantage/fibernow/service/TicketServiceImpl.java
@@ -1,15 +1,48 @@
 package eu.advantage.fibernow.service;
 
+import eu.advantage.fibernow.converter.DomainToDtoConverter;
+import eu.advantage.fibernow.converter.DtoToDomainConverter;
 import eu.advantage.fibernow.dto.TicketDto;
+import eu.advantage.fibernow.exception.BusinessException;
+import eu.advantage.fibernow.model.Ticket;
+import eu.advantage.fibernow.model.enums.TicketStatus;
+import eu.advantage.fibernow.repository.ITicketRepository;
+import jakarta.inject.Inject;
 
 import java.time.LocalDate;
 import java.util.List;
 
+import static eu.advantage.fibernow.exception.ExceptionStatus.BZ_ERROR_1001;
+import static eu.advantage.fibernow.util.JPAHelper.*;
+import static eu.advantage.fibernow.util.JPAHelper.closeEntityManager;
+
 public class TicketServiceImpl implements TicketService{
 
+    @Inject
+    private ITicketRepository ticketRepository;
     @Override
     public TicketDto saveTicket(TicketDto dto) {
-        return null;
+        Ticket ticket = DtoToDomainConverter.toDomain(dto);
+        beginTransaction();
+        try {
+            if (ticket.getId() == null) {
+                ticketRepository.create(ticket);
+            }
+            else {
+                Ticket found = ticketRepository.findById(ticket.getId());
+                if (found == null) {
+                    throw new BusinessException(BZ_ERROR_1001, ticket.getId());
+                }
+                ticketRepository.update(ticket);
+            }
+            commitTransaction();
+        } catch (Exception e) {
+            rollbackTransaction();
+            // rethrow exception
+        } finally {
+            closeEntityManager();
+        }
+        return DomainToDtoConverter.toDto(ticket);
     }
 
     @Override
@@ -24,6 +57,27 @@ public class TicketServiceImpl implements TicketService{
 
     @Override
     public TicketDto deleteTicket(TicketDto dto) {
-        return null;
+        Ticket ticket = DtoToDomainConverter.toDomain(dto);
+        beginTransaction();
+        Ticket found = null;
+        try {
+            found = ticketRepository.findById(ticket.getId());
+            if (found == null) {
+                throw new BusinessException(BZ_ERROR_1001, ticket.getId());
+            }
+
+            //Check if the Customer has open Tickets
+
+            //Soft Delete
+            found.setStatus(TicketStatus.DELETED);
+            ticketRepository.update(found);
+            commitTransaction();
+        } catch (Exception e) {
+            rollbackTransaction();
+            // rethrow exception
+        } finally {
+            closeEntityManager();
+        }
+        return DomainToDtoConverter.toDto(found);
     }
 }
